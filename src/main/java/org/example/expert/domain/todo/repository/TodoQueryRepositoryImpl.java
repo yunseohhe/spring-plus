d78ea2355e6fe5a9eb0c6374fba7ae29ced43af6
yunseohhe <dbstj4566@naver.com>
2024-10-10 19:53:43 +0900

    Level.2 [7.N+1]

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoQueryRepositoryImpl.java
@@ -0,0 +1,35 @@
+package org.example.expert.domain.todo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+import static org.example.expert.domain.todo.entity.QTodo.todo;
+
+@Repository
+@RequiredArgsConstructor
+public class TodoQueryRepositoryImpl implements TodoQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+//    public TodoQueryRepositoryImpl(JPAQueryFactory queryFactory) {
+//        this.queryFactory = queryFactory;
+//    }
+
+    @Override
+    public Optional<Todo> findByIdWithUser(Long todoId) {
+
+//        Todo result = queryFactory
+//                .selectFrom(todo)
+//                .leftJoin(todo.user, user).fetchJoin()
+//                .where(todo.id.eq(todoId))
+//                .fetchOne();
+
+        return null;
+//                Optional.ofNullable(result);
+    }
+
+}
--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -11,15 +11,16 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoQueryRepository {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+//    JPQL을 사용한 메서드 주석처리 함
+//    @Query("SELECT t FROM Todo t " +
+//            "LEFT JOIN t.user " +
+//            "WHERE t.id = :todoId")
+//    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
     // weather와 기간이 모두 있을 경우
     @Query("SELECT t FROM Todo t WHERE t.weather = :weather AND t.modifiedAt BETWEEN :startDate AND :endDate ORDER BY t.modifiedAt DESC")
